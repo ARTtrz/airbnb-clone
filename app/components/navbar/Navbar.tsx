88d81306f4f9ad68c152dbef9f86e6c731cab86f
Artyom <@artemkim953@gmail.com>
2023-06-26 20:37:31 +0600

    created categories

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,6 +7,7 @@ import Search from "./Search"
 import UserMenu from "./UserMenu"
 import {FC} from 'react'
 import { SafeUser } from "@/app/types"
+import Categories from "./Categories"
 interface NavbarProps{
     currentUser?: SafeUser | null
 }
@@ -25,6 +26,7 @@ const Navbar: FC<NavbarProps> = ({currentUser}) => {
                     
                 </Container>
             </div>
+            <Categories/>
         </div>
     )
 }
